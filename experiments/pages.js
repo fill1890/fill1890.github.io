38a2282cb6e8c2d8696fa62ddaf81e6506e94c7d
Andrew <Andrew@Andrews-MacBook-Air.local>
2015-09-09 16:03:50 +1000

    Cleanup
    Removed unnecessary files

--- experiments/pages.js
+++
@@ -1,9 +0,0 @@
-var pages = ['','' +
-'<?xml version="1.0" encoding="utf-8"?>' +
-'<!-- Generator: Adobe Illustrator 18.1.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->' +
-'<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve" class="backArrow">' +
-'<line fill="none" stroke="#222222" stroke-width="4" stroke-linecap="round" stroke-miterlimit="10" x1="2" y1="20" x2="38" y2="20"/>' +
-'<line fill="none" stroke="#222222" stroke-width="4" stroke-linecap="round" stroke-miterlimit="10" x1="2" y1="20" x2="20" y2="2"/>' +
-'<line fill="none" stroke="#222222" stroke-width="4" stroke-linecap="round" stroke-miterlimit="10" x1="2" y1="20" x2="20" y2="38"/>' +
-'</svg>'
-,'','','','','','','','','','','','','']
