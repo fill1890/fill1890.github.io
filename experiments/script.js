1acdafd9e0407701209115952cd9f18b50e21c5f
Andrew <Andrew@Andrews-MacBook-Air.local>
2015-09-17 14:13:30 +1000

    Bug fixes
    Tweaked JS styling for increased compatibility and fixed rotation by
    tiles.

--- experiments/script.js
+++ experiments/script.js
@@ -6,9 +6,11 @@ var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sa
     scroll = 0,
     select = '',
     scroll2 = 0,
-    carouselRotate = 0;
+    carouselRotate = 0,
+    prefixProps = {};
 
-function toggleRotate() {
+/*function toggleRotate() {
+    "use strict";
     var elemLeft = document.querySelectorAll(".block.left");
     var elemRight = document.querySelectorAll(".block.right");
     if (elemLeft[0].style.transform == "rotateY(90deg)") {
@@ -36,9 +38,9 @@ function toggleRotate() {
         }
         loopIn(0)
     }
-};
+};*/
 
-function rotateTo(degrees) {
+/*function rotateTo(degrees) {
     var distOut = (window.innerWidth + Math.sqrt(2) * window.innerWidth) / 2;
     var carousel = document.querySelector(".carousel"),
         carouselParent = document.querySelector(".carouselParent");
@@ -50,14 +52,16 @@ function rotateTo(degrees) {
     setTimeout(function () {
         carouselParent.style.transform = "translateZ(0)";
     }, 1000);
-}
+}*/
 
 function print(text) {
+    "use strict";
     console.log(text);
     return;
 }
 
 function array(data) {
+    "use strict";
     var o = {},
         i = 0;
     for (i = 0; i < data.length; i = i + 1) {
@@ -67,6 +71,7 @@ function array(data) {
 }
 
 function $$(selector) {
+    "use strict";
     // Wrapper for $(selector).elems
     return $(selector).elems;
 }
@@ -74,30 +79,25 @@ function $$(selector) {
 /* Javascript */
 
 function analogBegin() {
-    var secondMarkers = $$('.analogSecondMarkers circle'),
+    "use strict";
+    var secondMarkers = $('.analogSecondMarkers circle').elems,
         loop = 0,
-        hourMarkers = $$('.analogHourMarkers g'),
+        hourMarkers = $('.analogHourMarkers g').elems,
         hLoop = 0,
         i = 0;
     for (i = 0; i < secondMarkers.length; i = i + 1) {
-        secondMarkers[i].style['-webkit-transform'] = "rotate(" + loop + "deg)";
-        secondMarkers[i].style['-moz-transform'] = "rotate(" + loop + "deg)";
-        secondMarkers[i].style['-ms-transform'] = "rotate(" + loop + "deg)";
-        secondMarkers[i].style['-o-transform'] = "rotate(" + loop + "deg)";
-        secondMarkers[i].style.transform = "rotate(" + loop + "deg)";
+        $(secondMarkers[i]).css('transform', "rotate(" + loop + "deg)");
         loop += 30;
     }
-    for (i = 0; i < secondMarkers.length; i = i + 1) {
-        hourMarkers[i].style['-webkit-transform'] = "rotate(" + hLoop + "deg)";
-        hourMarkers[i].style['-moz-transform'] = "rotate(" + hLoop + "deg)";
-        hourMarkers[i].style['-ms-transform'] = "rotate(" + hLoop + "deg)";
-        hourMarkers[i].style['-o-transform'] = "rotate(" + hLoop + "deg)";
-        hourMarkers[i].style.transform = "rotate(" + hLoop + "deg)";
+    console.log(hourMarkers.length);
+    for (i = 0; i < hourMarkers.length; i = i + 1) {
+        $(hourMarkers[i]).css('transform', "rotate(" + hLoop + "deg)");
         hLoop += 30;
     }
 }
 
 function checkTime(i) {
+    "use strict";
     if (i < 10) {
         i = "0" + i;
     }
@@ -105,6 +105,7 @@ function checkTime(i) {
 }
 
 function checkTimeH(i) {
+    "use strict";
     if (i > 12) {
         i = i - 12;
     }
@@ -115,6 +116,7 @@ function checkTimeH(i) {
 }
 
 function checkAnalog(h, m, s) {
+    "use strict";
     if (s === 0) {
         if (($('.analogSecond').css("transform")) === 'rotate(354deg)') {
             $('.analogSecond').css("transform", "rotate(360deg)");
@@ -142,25 +144,26 @@ function checkAnalog(h, m, s) {
             }, 450);
         }
     } else {
-        $('.analogMinute').css("-webkit-transform", "rotate(" + (m * 6) + "deg)");
+        $('.analogMinute').css("transform", "rotate(" + (m * 6) + "deg)");
     }
     if (h === 12) {
         if (($('.analogHour').css("transform") === 'rotate(330deg)')) {
-            $('.analogHour').css("-webkit-transform", "rotate(360deg");
+            $('.analogHour').css("transform", "rotate(360deg");
             setTimeout(function () {
-                $('.analogHour').css("-webkit-transition", "none");
-                $('.analogHour').css("-webkit-transform", "rotate(0deg)");
+                $('.analogHour').css("transition", "none");
+                $('.analogHour').css("transform", "rotate(0deg)");
                 setTimeout(function () {
-                    $('.analogHour').css("-webkit-transition", "all 0.45s");
+                    $('.analogHour').css("transition", "all 0.45s");
                 }, 50);
             }, 450);
         }
     } else {
-        $('.analogHour').css("-webkit-transform", "rotate(" + (h * 30) + "deg)");
+        $('.analogHour').css("transform", "rotate(" + (h * 30) + "deg)");
     }
 }
 
 function time() {
+    "use strict";
     var today = new Date(),
         h = today.getHours(),
         m = today.getMinutes(),
@@ -179,6 +182,7 @@ function time() {
 
 
 function date() {
+    "use strict";
     var today = new Date(),
         d = today.getDay(),
         mo = today.getMonth(),
@@ -191,12 +195,16 @@ function date() {
 /* Custom Library Functions */
 
 (function () {
+    "use strict";
     var Library = function (selector) {
             // Set selection to specified selector
-            if (selector != null) {
+            if (selector !== null && typeof (selector) === "string") {
                 var select = document.querySelectorAll(selector);
                 this.elem = select[0];
                 this.elems = select;
+            } else if (selector !== null && typeof (selector) === "object") {
+                this.elem = selector;
+                this.elems = [selector];
             } else {
                 this.elem = null;
                 this.elems = null;
@@ -222,6 +230,17 @@ function date() {
             return this;
         },
         css: function (property, value) {
+            var prop = prefixProps[property] || getPrefix(property),
+                i = 0;
+            this.elems = this.elems || [this.elem];
+            if (value) {
+                for (i = 0; i < this.elems.length; i = i + 1) {
+                    this.elems[i].style[prop] = value;
+                }
+            } else {
+                return this.elem.style[prop];
+            }
+            /*
             var i = 0,
                 prefixes = ['-webkit-', '-moz-', '-ms-', '-o-', ''],
                 required = ['transform', 'transform-origin', 'transition'];
@@ -270,18 +289,22 @@ function date() {
                     return this.elem.style[property];
                 }
 
-            }
+            }*/
         }
     };
     if (!window.$) {
         window.$ = $;
     }
-})();
+}());
 
 (function () {
-    tiles = document.querySelectorAll('.tile');
-    pages = document.querySelectorAll('.page');
-    for (var i = 0; i < tiles.length; i++) {
+    "use strict";
+    var tileNodes = document.querySelectorAll('.tile'),
+        tiles = Array.prototype.slice.call(tileNodes),
+        pageNodes = document.querySelectorAll('.page'),
+        pages = Array.prototype.slice.call(pageNodes),
+        i = 0;
+    /*for (i = 0; i < tiles.length; i = i + 1) {
         tiles[i].addEventListener('click', function (e) {
             e = e || window.event;
             var target = e.target || e.srcElement,
@@ -293,35 +316,60 @@ function date() {
             var target = e.target || e.srcElement,
                 style = target.getAttribute("style"),
                 transform = style.substring(style.indexOf('transform:'), style.length);
-            transform = transform.substring(0, transform.indexOf(";"))
+            transform = transform.substring(0, transform.indexOf(";"));
             var rotate = transform.substring(transform.indexOf('rotateY(') + 8, transform.indexOf('deg)'));
             rotateCarousel(rotate);
         });
-    }
+    }*/
+    tiles.forEach(function (tile) {
+        tile.addEventListener('click', function (e) {
+            e = e || window.event;
+            var target = e.target || e.srcElement,
+                rotate = target.getAttribute("data-rotate");
+            rotateCarousel(rotate);
+        });
+    });
+    pages.forEach(function (page) {
+        page.addEventListener('click', function (e) {
+            e = e || window.event;
+            var target = e.target || e.srcElement,
+                transform = $(target).css('transform'),
+                pos = transform.indexOf('rotateY('),
+                rotateStr = transform.substr(pos + 8, 3),
+                rotate = parseInt(rotateStr, 10);
+            console.log(pos);
+            console.log(rotateStr);
+            console.log(rotate);
+            rotateCarousel(rotate);
+        });
+    });
     setTimeout(function () {
         analogBegin();
         time();
         date();
     }, 50);
-})();
+}());
 
 function toggleCss(params, select, delay) {
+    "use strict";
     delay = delay || 0;
     var selectedElems = document.querySelectorAll(select);
 
     function toggle(j, selectedElems) {
-        for (var i = 0; i < params.length; i++) {
+        var i = 0;
+        params.forEach(function (param, i, params) {
             var props = params[i].split(":"),
                 currentStyle = selectedElems[j].getAttribute('style') || "",
-                stylePos = currentStyle.indexOf(props[1]);
+                stylePos = currentStyle.indexOf(props[1]),
+                elem;
             if (stylePos < 0) {
                 elem = $(null);
                 elem.elem = selectedElems[j];
-                elem.css(props[0], props[1])
+                elem.css(props[0], props[1]);
             } else {
                 elem = $(null);
                 elem.elem = selectedElems[j];
-                elem.css(props[0], props[2])
+                elem.css(props[0], props[2]);
             }
             if (j < selectedElems.length - 1) {
                 setTimeout(function () {
@@ -329,26 +377,21 @@ function toggleCss(params, select, delay) {
                 }, delay);
             }
 
-        }
+        });
     }
     setTimeout(function () {
         toggle(0, selectedElems);
-    }, delay)
+    }, delay);
     return;
 
 }
 
 function menuScale() {
-    var shiftAway = new Array(
-            'transform:translate3d(25%, 0px, -150vw):translate3d(0,0,0)'
-        ),
-        changeColor = new Array(
-            'fill:#ffffff:#000000'
-        ),
-        overflow = new Array('overflow:hidden:visible'),
-        scale = new Array(
-            'transform:scale(1, 1):scale(0, 0)'
-        )
+    "use strict";
+    var shiftAway = ['transform:translate3d(25%, 0px, -150vw):translate3d(0,0,0)'],
+        changeColor = ['fill:#ffffff:#000000'],
+        overflow = ['overflow:hidden:visible'],
+        scale = ['transform:scale(1, 1):scale(0, 0)'];
     toggleCss(shiftAway, '.carouselMain');
     toggleCss(changeColor, '.menuButton rect');
     toggleCss(scale, '.tileContainer');
@@ -356,20 +399,21 @@ function menuScale() {
 }
 
 function rotateCarousel(deg) {
+    "use strict";
     var distOut = (window.innerWidth + Math.sqrt(2) * window.innerWidth) / 2;
-    deg = sanitizeDeg(parseInt(deg));
+    deg = sanitizeDeg(parseInt(deg, 10));
     if (Math.abs(carouselRotate - deg) > 180) {
         var oldTransition = $('.carousel').css('transition'),
             rotateOne = carouselRotate - 360,
             rotateTwo = carouselRotate + 360,
             newRotate = Math.abs(rotateOne - deg) < Math.abs(rotateTwo - deg) ? rotateOne : rotateTwo;
         $('.carousel').css('transition', 'all 0s');
-        setTimeout(function() {
-        $('.carousel').css('transform', 'translateZ(-' + distOut + 'px) rotateY(' + newRotate.toString() + 'deg)')
+        setTimeout(function () {
+            $('.carousel').css('transform', 'translateZ(-' + distOut + 'px) rotateY(' + newRotate.toString() + 'deg)');
         }, 5);
-        setTimeout(function() {
+        setTimeout(function () {
             $('.carousel').css('transition', oldTransition);
-        }, 10)
+        }, 10);
     }
     carouselRotate = deg;
     setTimeout(function () {
@@ -378,6 +422,7 @@ function rotateCarousel(deg) {
 }
 
 function sanitizeDeg(deg) {
+    "use strict";
     var pairs = {
         0: 0,
         45: 315,
@@ -388,8 +433,27 @@ function sanitizeDeg(deg) {
         270: 90,
         315: 45,
         360: 360
-    }
+    };
     console.log(deg);
     console.log(pairs[deg]);
     return pairs[deg];
+}
+
+function getPrefix(prop) {
+    "use strict";
+    var prefixes = ['', '-webkit-', '-moz-', '-ms-', '-o-'],
+        found = false,
+        i = 0;
+    while (found === false && i < prefixes.length) {
+        if (document.body.style.hasOwnProperty(prefixes[i] + prop)) {
+            prefixProps[prop] = prefixes[i] + prop;
+            found = true;
+        }
+        i = i + 1;
+    }
+    if (found === true) {
+        return prefixes[i - 1] + prop;
+    } else {
+        return null;
+    }
 }
